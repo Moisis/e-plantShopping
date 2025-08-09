9e8494af023994b0f3ff38629b6489257c388171
Moisis <moisis.george96@gmail.com>
2025-08-09 09:00:34 +0300

    added Task2 : State Management using Redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+      const {  name, image ,cost } = action.payload;
+      // Check if the item already exists in the cart
+      const existingItem = state.items.find(i => i.name === name);
+      if (existingItem) {
+        // If it exists, update the quantity
+        existingItem.quantity++;
+      } else {
+        // If it doesn't exist, add it to the cart
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     
     },
     removeItem: (state, action) => {
+      // Remove the item from the cart
+      state.items = state.items.filter(i => i.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      // Find the item in the cart
+      const existingItem = state.items.find(i => i.name === name);
+      if (existingItem) {
+        // If it exists, update the quantity
+        existingItem.quantity = quantity;
+      }
     },
   },
 });
